ddfed84d573d84cd530d64c8ca02f035985e6abc
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-05-19 14:13:19 -0700

    install sass

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Accordion from "./components/Accordion";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Accordion />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Accordion/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+function Accordion() {
+  return (
+    <div>
+      <div className={styles.element}>1</div>
+    </div>
+  );
+}
+
+export default Accordion;
